1e71780af53ba266e1da40fc398ee5b9ce57d091
raminrez <ramin12207@gmail.com>
2019-09-30 13:22:25 +0330

    remove deefault stylings

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
@@ -13,24 +12,7 @@ const App: React.FC = () => {
       .catch(e => console.log("axi error", e.message));
   }, []);
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'></div>;
 };
 
 export default App;
